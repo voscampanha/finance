1b0bf4361b3a464c232defc49c63d41fcdfc9eb8
voscampanha <vos.campanha@gmail.com>
2019-09-09 16:33:43 -0300

    allow any origin

--- src/main/java/com/finance/security/CorsConfig.java
+++ src/main/java/com/finance/security/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
