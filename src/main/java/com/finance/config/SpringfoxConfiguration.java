96e31cd3fd1f5889eac5f475ea4a95ade822c663
voscampanha <vos.campanha@gmail.com>
2019-09-12 09:57:42 -0300

    swagger with oauth2 configuration

--- src/main/java/com/finance/config/SpringfoxConfiguration.java
+++ src/main/java/com/finance/config/SpringfoxConfiguration.java
@@ -1,24 +1,52 @@
 package com.finance.config;
 
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.AuthorizationCodeGrantBuilder;
+import springfox.documentation.builders.OAuthBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.ResourceOwnerPasswordCredentialsGrant;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
+import springfox.documentation.service.TokenEndpoint;
+import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
 
 
 @Configuration
 public class SpringfoxConfiguration {
+	private static final String AUTH_SERVER = "http://localhost:8080";
+
+	@Value("${oauth.clientId}")
+	private String clientID;
+	
+	@Value("${oauth.secret}")
+	private String secret;
+	
+	
+	
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select()
             .paths(PathSelectors.regex("/api/.*"))
-            .build().apiInfo(apiEndPointsInfo());
+            .build()
+            .securitySchemes(Arrays.asList(securityScheme()))
+            .securityContexts(Arrays.asList(securityContext()))
+    		.apiInfo(apiEndPointsInfo());
     }
     private ApiInfo apiEndPointsInfo() {
         return new ApiInfoBuilder().title("Spring Boot REST API")
@@ -29,4 +57,41 @@ public class SpringfoxConfiguration {
             .version("1.0.0")
             .build();
     }
+    
+    @Bean
+    public SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+            .clientId(clientID)
+            .clientSecret(secret)
+            .scopeSeparator(" ")
+            .useBasicAuthenticationWithAccessCodeGrant(true)
+            .build();
+    }
+    
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+          .securityReferences(
+            Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+          .forPaths(PathSelectors.regex("/api/.*"))
+          .build();
+    }
+    
+    private SecurityScheme securityScheme() {
+        GrantType grantType = new ResourceOwnerPasswordCredentialsGrant(AUTH_SERVER + "/oauth/token");
+     
+        SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+            .grantTypes(Arrays.asList(grantType))
+            .scopes(Arrays.asList(scopes()))
+            .build();
+        return oauth;
+    }
+    
+    private AuthorizationScope[] scopes() {
+        AuthorizationScope[] scopes = { 
+          new AuthorizationScope("read", "for read operations"), 
+          new AuthorizationScope("write", "for write operations"), 
+          new AuthorizationScope("foo", "Access foo API") };
+        return scopes;
+    }
+    	
 }
